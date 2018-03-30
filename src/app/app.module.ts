d64e0265ab22efabb83b4252358ce7dfabd8ca1b
tanayv <tvardha2@illinois.edu>
2018-03-30 19:16:25 -0400

    ui displayed absolute test if vr mode also shows up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 
 
+
+import { UiComponent} from './ui/ui.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UiComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ui/ui.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ui',
+  templateUrl: './ui.component.html',
+  styleUrls: ['./ui.component.css']
+})
+export class UiComponent implements OnInit {
+  title = 'ui';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    
+
+  }
+}
